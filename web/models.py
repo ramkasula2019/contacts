9835c2a77882864399218d96c3a252d2f2ebb60a
ramkasula2019 <ram.kasula@cotiviti.com>
2020-03-26 09:16:08 +0545

    day 2 : Contacts model created in web app

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contacts(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    address = models.CharField(max_length = 30)
+    number = models.IntegerField()
+    dob = models.IntegerField()
+
+def __str__(self):
+    return self.first_name
