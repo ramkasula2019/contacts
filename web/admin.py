3c7113cf12ddcb8591ac463240e8e8dbb6f08d83
ramkasula2019 <ram.kasula@cotiviti.com>
2020-04-06 09:03:15 +0545

    Many to Many relation created for conact and Label

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Contacts, ContactNumber
+from .models import Contacts, ContactNumber, Label
 
 admin.site.register(Contacts)
 admin.site.register(ContactNumber)
+admin.site.register(Label)
--- web/models.py
+++ web/models.py
@@ -2,12 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+
+class Label(models.Model):
+    ''' A label for multiple contact '''
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class Contacts(models.Model):
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 30)
-    address = models.CharField(max_length = 30)
-    #number = models.IntegerField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(null=True, blank=True)
+    address = models.CharField(max_length=30)
+    job_title = models.CharField(max_length=30, null=True, blank=True)
+    company = models.CharField(max_length=30, null=True, blank=True)
     dob = models.IntegerField()
+    label = models.ManyToManyField(Label)
+    #number = models.IntegerField()
 
     def __str__(self):
         return self.first_name
@@ -15,13 +28,14 @@ class Contacts(models.Model):
 
 class ContactNumber(models.Model):
     """A contact number for inidividual model
-    
+
     Arguments:
         models {[type]} -- [description]
     """
     contact = models.ForeignKey(Contacts, on_delete=models.CASCADE)
     number = models.IntegerField()
-    type = models.CharField(max_length=50,default="Primary", null=True, blank=True)
+    type = models.CharField(
+        max_length=50, default="Primary", null=True, blank=True)
 
     def __str__(self):
-        return f"{self.contact.first_name} {self.number} {self.type}" 
+        return f"{self.contact.first_name} {self.number} {self.type}"
